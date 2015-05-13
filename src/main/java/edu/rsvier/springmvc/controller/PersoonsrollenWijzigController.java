65803bbd488824596a8cc99017ed0cbbabb26098
WiemerSteenbeek <wiemer.steenbeek@gmail.com>
2015-05-13 14:47:53 +0200

    PersoonRolPaginaData object toegevoegd. Gaat nog wel ff duren voordat de koppeling met de database is volbracht....

---
+++ src/main/java/edu/rsvier/springmvc/controller/PersoonsrollenWijzigController.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rsvier.springmvc.controller;
+
+import edu.rsvier.springmvc.model.Persoon;
+import edu.rsvier.springmvc.model.PersoonRolPaginaData;
+import edu.rsvier.springmvc.model.Persoonsrol;
+import edu.rsvier.springmvc.model.Rol;
+import edu.rsvier.springmvc.service.PersoonService;
+import edu.rsvier.springmvc.service.PersoonsrolService;
+import edu.rsvier.springmvc.service.RolService;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PersoonsrollenWijzigController {
+
+    @Autowired
+    PersoonService service;
+    
+    @Autowired
+    RolService rolService;
+    
+    @Autowired
+    PersoonsrolService persoonsrolService;
+    
+    @RequestMapping(value = {"{persoonid}-persoonsrollen"}, method = RequestMethod.GET)
+    public String getPersoon(@PathVariable int persoonid, PersoonRolPaginaData persoonRolData, ModelMap model) {
+        Persoon persoon = service.read(persoonid);
+        List<Rol>rollen = rolService.getAll();
+        List<String>rolnamen = new ArrayList<String>();
+        for(Rol rol: rollen){
+            rolnamen.add(rol.getNaam());
+        }
+        persoonRolData.setPersoon(persoon);
+        model.addAttribute("rolnamen", rolnamen);
+        model.addAttribute("persoonroldata", persoonRolData);
+        
+       
+        return "persoonsrollenwijzigen";
+    }
+    
+    @RequestMapping(value = {"{persoonid}-persoonsrollen"}, method = RequestMethod.POST)
+    public String wijzigPersoonPost(@PathVariable int persoonid, PersoonRolPaginaData persoonRolData, ModelMap model) {
+        Persoon persoon = service.read(persoonid);
+        
+        
+        List<String> rollenDezePersoon = persoonRolData.getPersoonsrolnamen();
+        System.out.println(rollenDezePersoon);
+        
+         model.addAttribute("succes", persoon.getVoornaam() + " "
+                + persoon.getAchternaam() + " is gewijzigd");
+        return "bevestigingspagina";
+    }
+    
+   
+    
+    
+
+}
+
---
+++ src/main/java/edu/rsvier/springmvc/model/PersoonRolPaginaData.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rsvier.springmvc.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author Wiemer
+ */
+public class PersoonRolPaginaData {
+    
+    private Persoon persoon = new Persoon();
+    
+    private List<String> persoonsrolNamen = new ArrayList<String>();
+
+    public List<String> getPersoonsrolnamen() {
+        return persoonsrolNamen;
+    }
+
+    public void setPersoonsrolnamen(List<String> persoonsrolNamen) {
+        
+        this.persoonsrolNamen = persoonsrolNamen;
+    }
+
+    public Persoon getPersoon() {
+        return persoon;
+    }
+
+    public void setPersoon(Persoon persoon) {
+        this.persoon = persoon;
+    }
+
+    
+    
+    
+}
